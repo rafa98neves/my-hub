09fea88166bc25258f6b15808206feeeff60a6dd
Rafael Neves <rafael.neves@unbabel.com>
2025-01-26 15:38:35 +0000

    Added bootstrap into projects

--- shell/src/pages/layout/layout.tsx
+++ shell/src/pages/layout/layout.tsx
@@ -8,13 +8,14 @@ import './layout.scss';
 // Types
 import { IChildren } from '../../types/layout';
 import Sidebar from '../../components/sidebar/sidebar';
+import Loader from '../../components/loader/loader';
 
 export default function Layout(props: { childrens: IChildren[] }) {
   return (
-    <section className="container">
+    <section className="layout-container">
       <Sidebar childrens={props.childrens} />
 
-      <React.Suspense fallback={null}>
+      <React.Suspense fallback={<Loader />}>
         <Routes>
           {props.childrens.map(child => {
             return <Route key={child.key} path={'/' + child.key} element={child.component} />
